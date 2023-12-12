04393d7eaf4cc5278c337ac03b9db292e34ec054
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-12-12 12:41:07 -0800

    Update build script to print last command on error.
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- build.cmd
+++ build.cmd
@@ -17,10 +17,13 @@ setlocal EnableExtensions EnableDelayedExpansion
         echo [command]!_command!
     )
     !_command!
-endlocal & exit /b %ERRORLEVEL%
+endlocal & (
+    set "SYSTEM_INFORMER_LAST_COMMAND=%_command%"
+    exit /b %ERRORLEVEL%
+)
 
 :$Main
-setlocal EnableExtensions
+setlocal EnableExtensions EnableDelayedExpansion
     call :ClearError
 
     set SYSTEM_INFORMER_CI=1
@@ -37,17 +40,17 @@ setlocal EnableExtensions
     call :Command "%~dp0build\build_sdk.cmd"
     if errorlevel 1 goto:$MainError
 
-    call :Command "%~dp0build\build_zdriver.cmd" prefast debug
+    call :Command "%~dp0build\build_zdriver.cmd" debug
     if errorlevel 1 goto:$MainError
 
-    call :Command "%~dp0build\build_zdriver.cmd" prefast release
+    call :Command "%~dp0build\build_zdriver.cmd" release
     if errorlevel 1 goto:$MainError
 
     echo Builds all completed successfully!
     goto:$MainDone
 
     :$MainError
-    echo [ERROR] Build failed. Error level: '%ERRORLEVEL%'
+    echo [ERROR] Build failed. Last command: "!SYSTEM_INFORMER_LAST_COMMAND!" [Error: %ERRORLEVEL%]
 
     :$MainDone
 endlocal & exit /b %ERRORLEVEL%
