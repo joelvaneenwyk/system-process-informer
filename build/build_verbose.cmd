807b2311557413ad7c64913acfe41edf23f3aa7e
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-12-11 16:54:49 -0800

    Simplified and improved the core build script
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- build/build_verbose.cmd
+++ build/build_verbose.cmd
@@ -2,7 +2,7 @@
 goto:$Main
 
 :Command
-setlocal EnableExtensions EnableDelayedExpansion
+setlocal EnableDelayedExpansion
     set "_command=%*"
     set "_command=!_command:      = !"
     set "_command=!_command:    = !"
@@ -14,12 +14,7 @@ setlocal EnableExtensions EnableDelayedExpansion
     set _error_value=%ERRORLEVEL%
 endlocal & exit /b %_error_value%
 
-:BuildExtra
-    setlocal EnableExtensions EnableDelayedExpansion
-    set _arch=%~1
-    set _path=%~2
-    set _target=%~3
-
+:SetDevEnv
     for /f "usebackq tokens=*" %%a in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
        set "VSINSTALLPATH=%%a"
     )
@@ -30,93 +25,73 @@ endlocal & exit /b %_error_value%
     )
 
     if not exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" goto:$BuildError
-    call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" %_arch%
+    call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" %~1
+exit /b
 
-    @cd /d "%~dp0\..\"
-    call :Command msbuild /m %_path% -t:Restore -property:Platform="%_target%" -verbosity:normal
-    if errorlevel 1 goto:$BuildError
-    call :Command msbuild /m %_path% -property:Configuration=Debug -property:Platform="%_target%" -verbosity:normal
-    if errorlevel 1 goto:$BuildError
-    call :Command msbuild /m %_path% -property:Configuration=Release -property:Platform="%_target%" -verbosity:normal
-    if errorlevel 1 goto:$BuildError
-    goto:$BuildEnd
+:BuildSolution %arch% %solution% %target%
+setlocal
+    goto:$BuildConfig
+    :BuildConfig
+       set "_config=%~1"
+       set "_target=%~2"
+       set "_msbuild_args=msbuild /m "%_solution%" -verbosity:normal -property:Configuration="%_config%" "
 
-    :$BuildError
-    echo [ERROR] Build failed: "%_path%"
-    goto:$BuildEnd
+       if "%_target%"=="" goto:$BuildConfigDefault
+           call :Command %_msbuild_args% -t:"Restore;Build" -property:Platform="%_target%"
+           if errorlevel 1 goto:$BuildConfigDone
+       goto:$BuildConfigDone
 
-    :$BuildEnd
-endlocal & exit /b %ERRORLEVEL%
+       :$BuildConfigDefault
+           call :Command %_msbuild_args% -property:Platform="Win32"
+           if errorlevel 1 goto:$BuildConfigDone
 
-:BuildExtras
-    setlocal
+           call :Command %_msbuild_args% -property:Platform="x64"
+           if errorlevel 1 goto:$BuildConfigDone
 
-    call :Build "amd64_x86" "tools\fixlib\fixlib.sln" "x86"
-    if errorlevel 1 goto:$BuildExtrasError
+           call :Command %_msbuild_args% -property:Platform="ARM64"
+           if errorlevel 1 goto:$BuildConfigDone
+       :$BuildConfigDone
+    exit /b %ERRORLEVEL%
+    :$BuildConfig
 
-    call :Build "amd64" "tools\GenerateZw\GenerateZw.sln" "Any CPU"
-    if errorlevel 1  goto:$BuildExtrasError
-    goto:$BuildExtras
+    set "_arch=%~1"
+    set "_solution=%~2"
+    set "_target=%~3"
 
-    :$BuildExtrasError
-    echo [ERROR] Build failed.
-    if not "%SYSTEM_INFORMER_CI%"=="1" (
-      pause
-    )
+    call :SetDevEnv "%_arch%"
 
-    :$BuildExtras
+    cd /d "%~dp0\..\"
+
+    call :BuildConfig "Debug" "%_target%"
+    if errorlevel 1 goto:$BuildSolutionDone
+
+    call :BuildConfig "Release" "%_target%"
+    if errorlevel 1 goto:$BuildSolutionDone
+
+    :$BuildSolutionDone
 endlocal & exit /b %ERRORLEVEL%
 
 :Build
-    set "_config=%~1"
-    call :Command msbuild /m SystemInformer.sln -property:Configuration=%_config% -property:Platform=Win32 -verbosity:normal
-    if %ERRORLEVEL% neq 0 goto:$BuildDone
+setlocal
+    call :BuildSolution "amd64_arm64" "SystemInformer.sln"
+    if errorlevel 1 goto:$BuildDone
 
-    call :Command msbuild /m SystemInformer.sln -property:Configuration=%_config% -property:Platform=x64 -verbosity:normal
-    if %ERRORLEVEL% neq 0 goto:$BuildDone
+    call :BuildSolution "amd64_arm64" "Plugins\Plugins.sln"
+    if errorlevel 1 goto:$BuildDone
 
-    call :Command msbuild /m SystemInformer.sln -property:Configuration=%_config% -property:Platform=ARM64 -verbosity:normal
-    if %ERRORLEVEL% neq 0 goto:$BuildDone
+    call :BuildSolution "amd64_x86" "tools\fixlib\fixlib.sln" "x86"
+    if errorlevel 1 goto:$BuildDone
 
-    call :Command msbuild /m Plugins\Plugins.sln -property:Configuration=%_config% -property:Platform=Win32 -verbosity:normal
-    if %ERRORLEVEL% neq 0 goto:$BuildDone
-
-    call :Command msbuild /m Plugins\Plugins.sln -property:Configuration=%_config% -property:Platform=x64 -verbosity:normal
-    if %ERRORLEVEL% neq 0 goto:$BuildDone
-
-    call :Command msbuild /m Plugins\Plugins.sln -property:Configuration=%_config% -property:Platform=ARM64 -verbosity:normal
-    if %ERRORLEVEL% neq 0 goto:$BuildDone
+    call :BuildSolution "amd64" "tools\GenerateZw\GenerateZw.sln" "Any CPU"
+    if errorlevel 1 goto:$BuildDone
 
     :$BuildDone
-exit /b
+endlocal & exit /b %ERRORLEVEL%
 
 :$Main
-    setlocal enableextensions
-    cd /d "%~dp0\..\"
-
-    for /f "usebackq tokens=*" %%a in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-       set "VSINSTALLPATH=%%a"
-    )
-
-    if not defined VSINSTALLPATH (
-       echo No Visual Studio installation detected.
-       goto:$MainError
-    )
-
-    if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
-       call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
-    ) else (
-       goto:$MainError
-    )
-
-    call :Build Debug
-    if %ERRORLEVEL% neq 0 goto:$MainError
-
-    call :Build Release
-    if %ERRORLEVEL% neq 0 goto:$MainError
-
-    call :BuildExtras
-    if %ERRORLEVEL% neq 0 goto:$MainError
+setlocal EnableExtensions
+    call :Build
+    if errorlevel 1 goto:$MainError
 
     echo Builds completed successfully!
     goto:$MainEnd
